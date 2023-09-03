6b3c9f4d37facdb7c2d4c728b7dea506e1492f5e
drogozhnikov <DitarRogozhnikov@gmail.com>
2023-09-03 13:16:54 +0300

    InitialCommit

---
+++ src/main/java/com/micro/i113_panda/I113PandaApplication.java
@@ -0,0 +1,13 @@
+package com.micro.i113_panda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class I113PandaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(I113PandaApplication.class, args);
+	}
+
+}
