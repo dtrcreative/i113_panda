f8722dfe34d556ca7a5d53c6b0ea4f67671b3f4e
drogozhnikov <DitarRogozhnikov@gmail.com>
2024-03-31 12:38:14 +0300

    Updated: appProperties

--- i113_panda_update.sh
+++ i113_panda_update.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-readonly service_name=i113_panda
-readonly service_image_name=panda-server
+readonly container_name=i113_panda
+readonly image_name=panda-server
 
-echo $service_name
-echo $service_image_name
+echo $container_name
+echo $image_name
 
 docker --version
 
-if docker ps | grep -- $service_name > /dev/null
+if docker ps -a | grep -- $container_name > /dev/null
 then
-docker stop $service_name
-docker rm $service_name
-docker rmi $service_image_name
+    docker stop $container_name
+    docker rm $container_name
+    docker rmi $image_name
 else
-echo no such container
+    echo no such container
 fi
